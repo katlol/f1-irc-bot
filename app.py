a28766d2fee74b76715ab3c1bbed4e51b0020687
Katia Esposito <katia@linux.com>
2023-10-07 14:29:05 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+import asyncio
+
+from irctokens import build, Line
+from ircrobots import Bot as BaseBot
+from ircrobots import Server as BaseServer
+from ircrobots import ConnectionParams
+
+SERVERS = [
+    ("libera", "irc.libera.chat")
+]
+
+class Server(BaseServer):
+    async def line_read(self, line: Line):
+        print(f"{self.name} < {line.format()}")
+        if line.command == "001":
+            print(f"connected to {self.isupport.network}")
+            await self.send(build("JOIN", ["#f1test"]))
+    async def line_send(self, line: Line):
+        print(f"{self.name} > {line.format()}")
+
+class Bot(BaseBot):
+    def create_server(self, name: str):
+        return Server(self, name)
+
+async def main():
+    bot = Bot()
+    for name, host in SERVERS:
+        params = ConnectionParams("f1test", host, 6697)
+        await bot.add_server(name, params)
+
+    await bot.run()
+
+if __name__ == "__main__":
+    asyncio.run(main())
